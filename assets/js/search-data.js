6380e7bd270898453d2c7fcb2c9bee6a99cc83a7
kMogs17 <kMogs17@users.noreply.github.com>
2025-11-20 04:53:45 +0000

    Deploying to gh-pages from @ kMogs17/kmogs17.github.io@10aea3813b38cbd64c58b9996fcf440b3ed3e688 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,6 +46,13 @@ ninja.data = [{
           window.open("mailto:%6B%6D%6F%67%61%6E%31%37@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/kyle-mogan", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
